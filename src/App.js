5e4746c70346bc4a6415d334cad746abc826150f
sumiit-jaiswal <sumitjaiswal263@gmail.com>
2024-08-05 04:14:10 +0530

    app.js changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react'
 
 import "./app.scss";
 
-import Header from "./components/header";
 import Jobs from './components/jobs';
 
 const App = () => {
   return (
     <div>
 
-      {/* <Header /> */}
       <Jobs />
 
     </div>
